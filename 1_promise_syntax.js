d3edf73621391a3a323f5e4dd9b83c9be8fd92d8
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-09-29 09:01:52 -0600

    Add notes

--- 1_promise_syntax.js
+++ 1_promise_syntax.js
@@ -1,4 +1,14 @@
 //Syntax
+// Let’s see what a promise looks like. 
+
+// - First we declare a variable called `promiseToTeachYouPromises`.
+// - We assign the `promiseToTeachYouPromises` variable to the promise object which
+//     - takes in a function
+//     - the function inside the promise object takes in two parameters: `resolve` and `reject`
+//     - inside the body of the function passed to the promise object, we define what the promise is
+// - First, we’ll define the variable `stephTaughtPromises` and set it to `true`
+// - Then we’ll write an `if condition` so that the promise `resolves` if I keep my promise and `rejects` if I do not
+//     - can pass anything you want to the resolve and reject methods
 const promiseToTeachYouPromises = new Promise((resolve, reject) => {
   let stephTaughtPromises = false;
   if (stephTaughtPromises) {
@@ -12,6 +22,12 @@ const promiseToTeachYouPromises = new Promise((resolve, reject) => {
   }
 });
 //Use
+// Let’s look at how we use promises. When we consume a promise, we use callback functions for the fulfilled and rejected states of that promise.
+
+// - We will call the `promiseToTeachYouPromises` function which returns a promise
+// - We will use the `.then()` method which takes in the result of the promise before it, then returns a promise
+// - In case the original promise is rejected, we will use the `.catch()` method to catch that error.
+// - Using the `.then` and `.catch` methods in tandem is called `promise chaining`
 promiseToTeachYouPromises
   .then((resultOfPromise) => {
     //do this when it succeeds
@@ -21,3 +37,10 @@ promiseToTeachYouPromises
     //do this when it fails
     console.error(error);
   });
+
+//   **Comprehension Questions:** 
+
+// 1. What is a promise?
+// 2. What are the three states of a promise?
+// 3. What does the `then` method do?
+// 4. What does the `catch` method do?
